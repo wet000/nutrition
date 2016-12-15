df25f0af0c05fedcfdf6c8c5bae37b7ef0848ffa
Michael West <westenterprisetechnologies001@gmail.com>
2016-12-15 06:46:51 -0600

    issue-4 Re-architected rest client in order to get auto-wiring in service to work

--- nutrition-rest/src/main/java/com/wet/api/nutrition/rest/driver/UsdaFoodCompositionRestClientTestDriver.java
+++ nutrition-rest/src/main/java/com/wet/api/nutrition/rest/driver/UsdaFoodCompositionRestClientTestDriver.java
@@ -3,22 +3,20 @@ package com.wet.api.nutrition.rest.driver;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.wet.api.nutrition.model.usda.json.UsdaReport;
+import com.wet.api.nutrition.model.usda.json.UsdaReportWrapper;
 import com.wet.api.nutrition.rest.UsdaFoodCompositionRestClient;
 
 public class UsdaFoodCompositionRestClientTestDriver 
 {
-	private UsdaFoodCompositionRestClient<UsdaReport> restClient;
+	private UsdaFoodCompositionRestClient<UsdaReportWrapper> restClient;
 	
 	@SuppressWarnings("unchecked")
 	public void run() 
 	{
-		System.out.println("Starting USDA Food Composition Rest Client Test Driver");
-		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/spring.xml");
-		restClient = (UsdaFoodCompositionRestClient<UsdaReport>)context.getBean("usdaFoodCompositionRestClientImpl");
-
-		UsdaReport usdaReport = restClient.getUsdaFoodReport("01009");
+		restClient = (UsdaFoodCompositionRestClient<UsdaReportWrapper>)context.getBean("usdaFoodCompositionRestClientImpl");
 		
+		UsdaReport usdaReport = restClient.getUsdaFoodReport("01009");
 		System.out.println("Food Name: " + usdaReport.getUsdaFood().getName());
 		
 		context.close();
