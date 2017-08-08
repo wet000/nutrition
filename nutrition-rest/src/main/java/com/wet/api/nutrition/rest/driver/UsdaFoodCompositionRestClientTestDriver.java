aa99e06c3ecc86fef3791bd7e6a41faa7e901b81
Michael West <westenterprisetechnologies001@gmail.com>
2017-08-08 11:51:21 -0500

    2017080811 - Code clean-up

--- nutrition-rest/src/main/java/com/wet/api/nutrition/rest/driver/UsdaFoodCompositionRestClientTestDriver.java
+++ nutrition-rest/src/main/java/com/wet/api/nutrition/rest/driver/UsdaFoodCompositionRestClientTestDriver.java
@@ -6,25 +6,25 @@ import com.wet.api.nutrition.model.usda.json.UsdaReport;
 import com.wet.api.nutrition.model.usda.json.UsdaReportWrapper;
 import com.wet.api.nutrition.rest.UsdaFoodCompositionRestClient;
 
-public class UsdaFoodCompositionRestClientTestDriver 
+public class UsdaFoodCompositionRestClientTestDriver
 {
-	private UsdaFoodCompositionRestClient<UsdaReportWrapper> restClient;
-	
-	@SuppressWarnings("unchecked")
-	public void run() 
-	{
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/spring.xml");
-		restClient = (UsdaFoodCompositionRestClient<UsdaReportWrapper>)context.getBean("usdaFoodCompositionRestClientImpl");
-		
-		UsdaReport usdaReport = restClient.getUsdaFoodReport("01009");
-		System.out.println("Food Name: " + usdaReport.getUsdaFood().getName());
-		
-		context.close();
-	}
-	
-	public static void main(String[] args) 
-	{
-		UsdaFoodCompositionRestClientTestDriver testDriver = new UsdaFoodCompositionRestClientTestDriver();
-		testDriver.run();
-	}
+    private UsdaFoodCompositionRestClient<UsdaReportWrapper> restClient;
+
+    @SuppressWarnings("unchecked")
+    public void run()
+    {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/spring.xml");
+        restClient = (UsdaFoodCompositionRestClient<UsdaReportWrapper>) context.getBean("usdaFoodCompositionRestClientImpl");
+
+        UsdaReport usdaReport = restClient.getUsdaFoodReport("01009");
+        System.out.println("Food Name: " + usdaReport.getUsdaFood().getName());
+
+        context.close();
+    }
+
+    public static void main(String[] args)
+    {
+        UsdaFoodCompositionRestClientTestDriver testDriver = new UsdaFoodCompositionRestClientTestDriver();
+        testDriver.run();
+    }
 }
--- nutrition-rest/src/main/java/com/wet/api/nutrition/rest/impl/UsdaFoodCompositionRestClientImpl.java
+++ nutrition-rest/src/main/java/com/wet/api/nutrition/rest/impl/UsdaFoodCompositionRestClientImpl.java
@@ -10,39 +10,39 @@ import com.wet.api.nutrition.rest.UsdaFoodCompositionRestClient;
 @Service
 public class UsdaFoodCompositionRestClientImpl extends AbstractSpringRestClient<UsdaReportWrapper> implements UsdaFoodCompositionRestClient<UsdaReportWrapper>
 {
-	private static final String METHOD_REPORTS = "reports";
-	private static final String PARAM_USDA_FOOD_ID = "ndbno";
-	private static final String PARAM_REPORT_TYPE = "type";
-	private static final String PARAM_FORMAT = "format";
-	private static final String PARAM_API_KEY = "api_key";
-	
-	private static final String REPORT_TYPE_FULL = "f";
-	private static final String FORMAT_JSON = "json";
-	private static final String API_KEY = "IzzZrz6wEbEmXUhk4vOohBmTYhgQebIQYlMR7W5v";
-	
-	private static final String BASE_URI = "http://api.nal.usda.gov/ndb/";
-	
-	@Override
-	public UsdaReport getUsdaFoodReport(String usdaFoodId) 
-	{
-		setMethod(METHOD_REPORTS);
-		putParam(PARAM_USDA_FOOD_ID, usdaFoodId);
-		putParam(PARAM_REPORT_TYPE, REPORT_TYPE_FULL);
-		putParam(PARAM_FORMAT, FORMAT_JSON);
-		putParam(PARAM_API_KEY, API_KEY);
-		
-		return get().getReport();
-	}
+    private static final String METHOD_REPORTS = "reports";
+    private static final String PARAM_USDA_FOOD_ID = "ndbno";
+    private static final String PARAM_REPORT_TYPE = "type";
+    private static final String PARAM_FORMAT = "format";
+    private static final String PARAM_API_KEY = "api_key";
 
-	@Override
-	protected void setType() 
-	{
-		this.type = UsdaReportWrapper.class;
-	}
+    private static final String REPORT_TYPE_FULL = "f";
+    private static final String FORMAT_JSON = "json";
+    private static final String API_KEY = "IzzZrz6wEbEmXUhk4vOohBmTYhgQebIQYlMR7W5v";
 
-	@Override
-	protected void setBaseUri() 
-	{
-		this.baseUri = BASE_URI;
-	}
+    private static final String BASE_URI = "http://api.nal.usda.gov/ndb/";
+
+    @Override
+    public UsdaReport getUsdaFoodReport(String usdaFoodId)
+    {
+        setMethod(METHOD_REPORTS);
+        putParam(PARAM_USDA_FOOD_ID, usdaFoodId);
+        putParam(PARAM_REPORT_TYPE, REPORT_TYPE_FULL);
+        putParam(PARAM_FORMAT, FORMAT_JSON);
+        putParam(PARAM_API_KEY, API_KEY);
+
+        return get().getReport();
+    }
+
+    @Override
+    protected void setType()
+    {
+        this.type = UsdaReportWrapper.class;
+    }
+
+    @Override
+    protected void setBaseUri()
+    {
+        this.baseUri = BASE_URI;
+    }
 }
